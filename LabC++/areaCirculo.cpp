b9b4714561d35137918bd30dcc6c8e397ed4016f
Javier Hernández <jhernandez9353@arecibointer.edu>
2024-03-18 09:18:18 -0400

    new file

---
+++ LabC++/areaCirculo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Definicion de variables
+    double area, radio;  // Es lo mismso que añadir double radio;
+    const double PI = 3.14 // Identificador de constante
+
+    cout <<"Ingrese la medida del radio: ";
+    cin >> radio;
+
+    area = PI * radio * radio;
+
+    cout <<"El area del circulo es: "<<area << endl; //endl es para dejar espacio en linea 
+
+    return 0;
+
+}
